d55f4eed69b8996bf07ba1a6c43324125c5cf67e
Efehan Ömer Cekic <Efehan.Cekic@Student.HTW-Berlin.de>
2024-05-07 15:57:43 +0200

    M2 fertig

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
-  <!DOCTYPE html>
-  <html>
-  <head>
-    <title>Marathon Tagebuch</title>
-  </head>
-  <body>
-  <h1>
-    Hello World!
-  </h1>
-  </body>
-  </html>
+  <div id="app">
+    <training-entries :entries="trainingEntries"></training-entries>
+  </div>
 </template>
+
+<script>
+import TrainingEntries from './components/TrainingEntries.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TrainingEntries
+  },
+  data() {
+    return {
+      trainingEntries: [
+        { id: 1, date: '2023-05-01', kilometers: 5, time: '30 min', goalAchieved: true },
+      ]
+    }
+  }
+}
+</script>
---
+++ src/components/TrainingEntries.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="training-entries">
+    <h2>Trainingsübersicht</h2>
+    <ul>
+      <li v-for="entry in entries" :key="entry.id">
+        <div class="entry">
+          <h3>{{ entry.date }}</h3>
+          <p>Gelaufene Kilometer: {{ entry.kilometers }} km</p>
+          <p>Benötigte Zeit: {{ entry.time }}</p>
+          <p>Ziel erreicht: <strong>{{ entry.goalAchieved ? 'Ja' : 'Nein' }}</strong></p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TrainingEntries',
+  props: {
+    entries: Array
+  }
+}
+</script>
