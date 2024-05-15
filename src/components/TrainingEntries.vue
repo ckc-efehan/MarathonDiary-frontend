689333cc4f3b537ff17818a45eaf9ff927beac19
Efehan Ömer Cekic <Efehan.Cekic@Student.HTW-Berlin.de>
2024-05-15 13:38:55 +0200

    axios added

--- src/components/TrainingEntries.vue
+++ src/components/TrainingEntries.vue
@@ -9,7 +9,7 @@
           <div class="entry">
             <h3>{{ entry.date }}</h3>
             <p>Ziel-Kilometer: <span class="bold-text"> {{ entry.goalKilometers }}</span></p>
-            <p>Gelaufene Kilometer:<span class="bold-text"> {{ entry.kilometers }} km </span></p>
+            <p>Gelaufene Kilometer: <span class="bold-text"> {{ entry.kilometers }} km </span></p>
             <p>Ziel-Zeit: <span class="bold-text"> {{ entry.goalTime }}</span></p>
             <p>Benötigte Zeit: <span class="bold-text"> {{ entry.time }}</span></p>
             <p>Ziel erreicht: <span class="bold-text">{{ entry.goalAchieved ? 'Ja' : 'Nein' }}</span></p>
