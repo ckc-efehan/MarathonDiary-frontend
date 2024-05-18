ba03d281e154611c53a807cd3bf91725f9e66714
Efehan Ömer Cekic <Efehan.Cekic@Student.HTW-Berlin.de>
2024-05-18 12:11:35 +0200

    navbar added

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,9 @@
-<template>
-  <div id="app">
-    <training-entries :entries="trainingEntries"></training-entries>
-  </div>
-</template>
+<template src="./views/AppTemplate.html"></template>
 
 <script>
 import axios from 'axios';
-import TrainingEntries from './components/TrainingEntries.vue'
+import './assets/appStyles.css';
+import TrainingEntries from './components/TrainingEntries.vue';
 
 export default {
   name: 'App',
@@ -34,70 +31,3 @@ export default {
   }
 }
 </script>
-
-<style>
-body {
-  background: linear-gradient(to right, #c9d6ff, #ffffff);
-  margin: 0;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-#app {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  min-height: 100vh;
-}
-
-.page-container {
-  align-self: center;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  width: 100vw;
-  background: inherit;
-}
-
-.training-entries {
-  background-color: #ffffff;
-  padding: 20px;
-  border-radius: 20px;
-  width: 20%;
-  box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.2);
-}
-
-.training-entries h2 {
-  color: #000000;
-  text-align: center;
-}
-
-.training-entries ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-.training-entries li {
-  margin-bottom: 10px;
-  border-bottom: 1px solid #ccc;
-  padding-bottom: 10px;
-}
-
-.entry h3 {
-  color: #007BFF;
-}
-
-.entry p {
-  color: #666;
-  margin: 5px 0;
-}
-
-.bold-text {
-  font-weight: bold;
-}
-</style>
-
--- src/components/TrainingEntries.vue
+++ src/components/TrainingEntries.vue
@@ -1,21 +1,18 @@
 <template>
-  <div class="page-container">
-    <div class="training-entries">
-      <h2>Trainingsübersicht</h2>
-      <br>
-      <ul>
-        <li v-for="entry in entries" :key="entry.date">
-          <div class="entry">
-            <h3>{{ entry.date }}</h3>
-            <p>Ziel-Zeit: <span class="bold-text">{{ entry.targetTime }} h</span></p>
-            <p>Ziel-Kilometer: <span class="bold-text">{{ entry.targetKilometre }} km</span></p>
-            <p>Gelaufene Kilometer: <span class="bold-text">{{ entry.kilometreRan }} km</span></p>
-            <p>Benötigte Zeit: <span class="bold-text">{{ entry.timeRan }} h</span></p>
-            <p>Ziel erreicht: <span class="bold-text">{{ entry.goalReached ? 'Ja' : 'Nein' }}</span></p>
-          </div>
-        </li>
-      </ul>
-    </div>
+  <div class="training-entries">
+    <h2>Trainingsübersicht</h2>
+    <ul>
+      <li v-for="entry in entries" :key="entry.date">
+        <div class="entry">
+          <h3>{{ entry.date }}</h3>
+          <p>Ziel-Zeit: <span class="bold-text">{{ entry.targetTime }} h</span></p>
+          <p>Ziel-Kilometer: <span class="bold-text">{{ entry.targetKilometre }} km</span></p>
+          <p>Gelaufene Kilometer: <span class="bold-text">{{ entry.kilometreRan }} km</span></p>
+          <p>Benötigte Zeit: <span class="bold-text">{{ entry.timeRan }} h</span></p>
+          <p>Ziel erreicht: <span class="bold-text">{{ entry.goalReached ? 'Ja' : 'Nein' }}</span></p>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -23,45 +20,24 @@
 export default {
   name: 'TrainingEntries',
   props: {
-    entries: Array
+    entries: {
+      type: Array,
+      required: true
+    }
   }
 }
 </script>
 
 <style scoped>
-body {
-  background: linear-gradient(to right, #c9d6ff, #ffffff);
-  margin: 0;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-#app {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  min-height: 100vh;
-}
-
-.page-container {
-  align-self: center;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  width: 100vw;
-  background: inherit;
-}
-
 .training-entries {
   background-color: #ffffff;
   padding: 20px;
   border-radius: 20px;
-  width: 20%;
+  max-width: 600px;
+  width: 100%;
   box-shadow: 0px 8px 16px rgba(0, 0, 0, 0.2);
+  overflow-y: auto;
+  max-height: 80vh;
 }
 
 .training-entries h2 {
