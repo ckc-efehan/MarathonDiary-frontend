3a7ff3111c31fd3896f2e42376ca50c283026a0a
Efehan <Efehan.Cekic@Student.HTW-Berlin.de>
2024-06-04 18:30:35 +0200

    Datum bei neuem Eintrag automatisch v4

--- src/components/TrainingEntries.vue
+++ src/components/TrainingEntries.vue
@@ -55,10 +55,7 @@ export default {
     },
     getCurrentDate(){
       const date = new Date();
-      const day = String(date.getDate()).padStart(2, '0');
-      const month = String(date.getMonth() + 1).padStart(2, '0');
-      const year = date.getFullYear();
-      return `${day}.${month}.${year}`;
+      return date.toISOString().slice(0,10);
     },
     submitEntry() {
       const newEntry = {
